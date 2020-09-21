d4e3df2e6f7b73196a3ccaedf963f566050a3f25
Vincent He <vincent0710@Outlook.com>
2020-09-21 11:59:58 -0400

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Vincent He on 2020-09-21.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
